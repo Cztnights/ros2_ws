31cdee7eba9ce9366809b8962867b57b05877d4e
Cztnights <harm.evert30@gmail.com>
2026-03-10 15:18:49 +0200

    ee

--- .bash_history
+++ .bash_history
@@ -41,3 +41,20 @@ ls
 ./build_ws.sh 
 ls
 exit
+ros2 run turtlesim turtlesim_node
+. build_ws.sh
+ros2 run turtlesim turtlesim_node
+. build_ws.sh 
+ros2 launch my_robot_controller draw_circle.launch.py 
+. build_ws.sh 
+ros2 run turtlesim turtlesim_node
+clear
+exit
+ros2 topic list
+ros2 topic info /turtle1/cmd_vel
+ros2 interface show geometry_msgs/msg/Twist
+ros2 run my_robot_controller draw_circle
+ros2 run my_robot_controller  move_circle 
+ros2 run my_robot_controller turtle_controller 
+clear
+. build_ws.sh 
--- src/my_robot_controller/setup.py
+++ src/my_robot_controller/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'my_robot_controller'
 
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -26,6 +28,7 @@ setup(
         'console_scripts': [
         'test_node = my_robot_controller.my_first_node:main',
         'move_circle = my_robot_controller.draw_circle:main',
+        'turtle_controller = my_robot_controller.turtle_controller:main',
         ],
     },
 )
